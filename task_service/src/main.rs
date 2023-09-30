08e1a3880d418a6d9e7712eafa494ecca8046744
KPMGE <jesuskevin254@gmail.com>
2023-09-30 12:34:17 -0300

    :sparkles: add simple healthcheck route

--- task_service/src/main.rs
+++ task_service/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, Responder, get, HttpResponse};
+
+#[get("/healthcheck")]
+async fn healthcheck() -> impl Responder {
+  HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| 
+    App::new().service(healthcheck)
+  )
+  .bind(("127.0.0.1", 3333))?
+  .run()
+  .await
 }
