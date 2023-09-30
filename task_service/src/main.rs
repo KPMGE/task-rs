1fb82efee31f835a7a6d7f7f8cc4af6216beb973
KPMGE <jesuskevin254@gmail.com>
2023-09-30 13:14:11 -0300

    :poop: implement dummy create_task route

--- task_service/src/main.rs
+++ task_service/src/main.rs
@@ -1,16 +1,30 @@
-use actix_web::{HttpServer, App, Responder, get, HttpResponse};
+use actix_web::{get, post, web::Json, App, HttpResponse, HttpServer, Responder};
+use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Task {
+    id: i32,
+    title: String,
+    description: String,
+    due_date: NaiveDate,
+}
 
 #[get("/healthcheck")]
 async fn healthcheck() -> impl Responder {
-  HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish()
+}
+
+#[post("/tasks")]
+async fn create_task(task: Json<Task>) -> impl Responder {
+    println!("task: {:?}", task);
+    HttpResponse::Ok().finish()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| 
-    App::new().service(healthcheck)
-  )
-  .bind(("127.0.0.1", 3333))?
-  .run()
-  .await
+    HttpServer::new(|| App::new().service(healthcheck).service(create_task))
+        .bind(("127.0.0.1", 3333))?
+        .run()
+        .await
 }
