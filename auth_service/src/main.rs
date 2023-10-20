8516de4db7889abfff5b02c67cfa656c031f4e08
KPMGE <jesuskevin254@gmail.com>
2023-10-20 05:12:11 -0300

    :poop: implement simple auth service, to be refactored

--- auth_service/src/main.rs
+++ auth_service/src/main.rs
@@ -1,3 +1,77 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::{Duration, Local};
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web_httpauth::extractors::bearer::BearerAuth;
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    iat: i64,
+    exp: i64,
+    sub: String,
+    name: String,
+}
+
+#[get("/login")]
+async fn login() -> impl Responder {
+    let now = Local::now();
+    let iat = now.timestamp();
+    let expiration_time = Duration::hours(1);
+    let exp = (now + expiration_time).timestamp();
+
+    let my_claims = Claims {
+        sub: "1".to_string(),
+        name: "test name".to_string(),
+        iat,
+        exp,
+    };
+
+    let jwt_secret = "super secret";
+
+    let token = match jsonwebtoken::encode(
+        &Header::default(),
+        &my_claims,
+        &EncodingKey::from_secret(jwt_secret.as_ref()),
+    )
+    .map_err(|e| {
+        eprintln!("ERROR: {}", e);
+        e
+    }) {
+        Ok(token) => token,
+        Err(_e) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    HttpResponse::Ok().json(token)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Params {}
+
+#[get("/test_token")]
+async fn test_token(auth: BearerAuth) -> impl Responder {
+    let token = auth.token();
+
+    let token_data = jsonwebtoken::decode::<Claims>(
+        token,
+        &DecodingKey::from_secret("super secret".as_ref()),
+        &Validation::default(),
+    )
+    .unwrap();
+
+    println!("claims: {:?}", token_data.claims);
+
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 3334;
+
+    println!("server listening on: http://127.0.0.1:{}", port);
+
+    HttpServer::new(move || App::new().service(login).service(test_token))
+        .bind(("127.0.0.1", port))?
+        .run()
+        .await
 }
